883eb74d625e0b0466834cf903e2fd91b5bb39d0
sinsohi_04 <sohi8451@naver.com>
2024-02-01 17:46:01 +0900

    9강 숙제

--- server.js
+++ server.js
@@ -43,3 +43,10 @@ app.get("/list", async (요청, 응답) => {
   // 응답.send(result[0].title);
   응답.render("list.ejs", { posts: result });
 });
+
+app.get("/time", (요청, 응답) => {
+  // 응답.send(new Date());
+  응답.render("time.ejs", { time: new Date() });
+});
+
+// new Date()
