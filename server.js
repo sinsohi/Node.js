cf2f51e706c309ad01dc2ad30d862c92f3b2a231
sinsohi_04 <sohi8451@naver.com>
2024-02-01 16:43:16 +0900

    7강 - MongoDB와 서버 연결하려면

--- server.js
+++ server.js
@@ -5,9 +5,24 @@ const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(8080, () => {
-  console.log("http://localhost:8080 에서 서버 실행중");
-});
+const { MongoClient } = require("mongodb");
+
+let db;
+const url =
+  "mongodb+srv://sosin_303:tlsthgml4033!@cluster0.zrw2oxx.mongodb.net/?retryWrites=true&w=majority";
+new MongoClient(url)
+  .connect()
+  .then((client) => {
+    console.log("DB연결성공");
+    db = client.db("forum");
+
+    app.listen(8080, () => {
+      console.log("http://localhost:8080 에서 서버 실행중");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (요청, 응답) => {
   // 누가 메인페이지 접속시
@@ -15,7 +30,10 @@ app.get("/", (요청, 응답) => {
 });
 
 app.get("/news", (요청, 응답) => {
-  응답.send("오늘 비옴"); // 이거 보내주셈
+  db.collection("post").insertOne({
+    title: "어쩌구",
+  });
+  //   응답.send("오늘 비옴"); // 이거 보내주셈
 });
 
 app.get("/about", (요청, 응답) => {
