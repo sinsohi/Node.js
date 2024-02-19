6cbb770174e1ecced5a16af32deb37874b621531
sinsohi_04 <sohi8451@naver.com>
2024-02-19 10:41:10 +0900

    43강 - 실시간 데이터 푸시 1 (Server sent events)

--- server.js
+++ server.js
@@ -360,3 +360,16 @@ io.on("connection", (socket) => {
     io.to(data.room).emit("message-broadcast", data.msg);
   });
 });
+
+app.get("/stream/list", (요청, 응답) => {
+  응답.writeHead(200, {
+    Connection: "keep-alive",
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+  });
+
+  setInterval(() => {
+    응답.write("event: msg\n");
+    응답.write("data: 바보\n\n");
+  }, 1000);
+});
